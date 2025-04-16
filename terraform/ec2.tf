011e4b090d9c5795ff0d95f77ecafa348dc8b85e
Roy Ruiz <royruiz083@gmail.com>
2025-04-16 13:16:57 +0200

    Small change to spin up 3 ec2 instances

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -42,7 +42,7 @@ resource "aws_instance" "app_instances" {
   instance_type = "t2.micro"
   key_name      = "EC2 Tutorial"
 
-  count = 2 # Number of EC2 instances
+  count = 3 # Number of EC2 instances
 
   root_block_device {
     volume_size = 30
