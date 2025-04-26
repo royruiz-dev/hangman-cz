96099f8aba7e3e64110bf715b4485af48486524d
Roy Ruiz <royruiz083@gmail.com>
2025-04-26 03:14:03 +0200

    Allowing all ips for ssh inbound, simplified action workflow

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -22,7 +22,8 @@ resource "aws_security_group_rule" "allow_ssh_inbound" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["${var.my_ip}"]
+  # cidr_blocks       = ["${var.my_ip}"]
+  cidr_blocks       = ["0.0.0.0/0"] # Allows all ip addresses
   security_group_id = aws_security_group.instances.id
 }
 
